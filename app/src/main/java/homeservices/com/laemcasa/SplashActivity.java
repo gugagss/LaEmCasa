da39719034b0148e14e0826f006305f1da7e9e9d
GUSTAVOGSS.DEV <gustavogss.dev@gmail.com>
2017-01-14 01:34:40 -0300

    Initial commit

---
+++ app/src/main/java/homeservices/com/laemcasa/SplashActivity.java
@@ -0,0 +1,16 @@
+package homeservices.com.laemcasa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mobiledeveloper on 31/12/16.
+ */
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}
